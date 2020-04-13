948d3182d266645900db13596737589b5b27e135
Leired7 <leirediez.7@gmail.com>
2020-04-13 12:47:46 +0200

    Set the state from a input

--- src/components/counterHook.jsx
+++ src/components/counterHook.jsx
@@ -8,6 +8,12 @@ function CounterHook() {
   return (
     <div>
       <h2>This counter use Hook {useState}</h2>
+      <input
+        type="number"
+        placeholder="Select new count init: "
+        value={count}
+        onChange={(event) => setCount(Number(event.target.value))}
+      ></input>
       <p>The counter is at: {count} </p>
       <button onClick={() => setCount(count + 1)}> +1</button>
       <button onClick={() => setCount(count - 1)}> -1 </button>
